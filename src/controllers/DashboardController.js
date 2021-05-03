d6881957e60f5b71b087b11e722a61b5b09bc5bb
Daniel Leutze <leutzed@gmail.com>
2021-05-03 02:24:42 +0000

    dashboard page working, now todo database

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -7,11 +7,26 @@ module.exports = {
         const jobs = Job.get();
         const profile = Profile.get();
 
-        //ajustes no job -> calculo do tempo restante
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        };
+
+        // total de horas por dia de cada job em progresso
+        let jobTotalHours = 0;
+
+        //ajustes no job -> calculo do tempo restante. map olha item a item
         const updatedJobs = jobs.map((job) => {
             const remaining = JobUtils.remainingDays(job);
-            const status = remaining <= 0 ? 'done' : 'progress'
-    
+            const status = remaining <= 0 ? 'done' : 'progress';
+
+            // somando quantidade de status -> ele pega o nome da chave do status e soma 1
+            statusCount[status] += 1;
+
+            // se o status for progress, soma com as horas diarias, se não, só retorna sem fazer nada
+            jobTotalHours = status == 'progress' ? jobTotalHours += Number(job["daily-hours"]) : jobTotalHours;
+
             return {
                 ...job,
                 remaining,
@@ -19,6 +34,10 @@ module.exports = {
                 budget: JobUtils.calculateBudget(job, profile["value-hour"])
             };
         })
-        return response.render("index", { jobs: updatedJobs });
+
+        // qtd de horas que quero trabalhar MENOS qtd de horas/dia em cada job, e statusCount == progress
+        const freeHours = profile["hours-per-day"] - jobTotalHours;
+
+        return response.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours });
     }
 }
