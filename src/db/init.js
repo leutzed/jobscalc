eb64270bbc3df1d787a33dcce066f717280dc742
Daniel Leutze <leutzed@gmail.com>
2021-05-10 15:33:35 +0000

    database working

--- src/db/init.js
+++ src/db/init.js
@@ -1,35 +1,59 @@
 const Database = require('./config');
 
-Database();
+const initDb = {
+    //esse async diz que vai ter await dentro do init()    
+    async init(){
 
-Database.exec(`CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly_budget INT,
-    days_per_week INT,
-    hours_per_day INT,
-    vacation_per_year INT,
-    value_hour INT
-)`);
+    const db = await Database();
 
-Database.exec(`CREATE TABLE jobs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily_hours INT,
-    total_hours INT,
-    created_at DATETIME
-)`);
+    await db.exec(`CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+    )`);
 
-Database.run(`INSERT INTO jobs(
-    name,
-    daily_hours,
-    total_hours)
-    VALUES (
-    "Job nome",
-    2,
-    5,
-    1617514376018)
-);`);
+    await db.exec(`CREATE TABLE jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME
+    )`);
 
-Database.close();
+    await db.run(`INSERT INTO jobs(
+        name,
+        daily_hours,
+        total_hours
+        ) VALUES (
+        "Job nome",
+        2,
+        5
+    );`);
+
+    await db.run(`INSERT INTO profile(
+        name,
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year
+        ) VALUES (
+        "Daniel",
+        "https://avatars.githubusercontent.com/u/55982817?v=4",
+        3000,
+        3,
+        2,
+        1
+    );`);
+
+    await db.close();
+
+    }
+}
+
+initDb.init();
