ec56c8c7a15fa5f5b446ecd7134e60bc35fd4251
leutzed <leutzed@gmail.com>
2021-06-14 17:21:03 -0300

    just deleting useless stuff which had remained from the refactor

--- src/server.js
+++ src/server.js
@@ -1,23 +1,3 @@
-// import express from 'express';
-// import routes from './routes';
-// const server = express();
-
-//usando template engine
-// server.set('view engine', 'ejs');
-
-// mudar localizacao da pasta views
-// nome da pasta, dirname, localizacao da pasta
-// server.set('views', path.join(__dirname, 'views'));
-
-// habilita os arquivos static
-// server.use(express.static("public"));
-
-// habilita o request.body
-// server.use(express.urlencoded({ extended: true }));
-
-// routes
-// server.use(routes);
-
 import app from './app';
 
 app.listen(3000, () => console.log('rodei'));
